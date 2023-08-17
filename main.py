ee8e6f1978e14b775161286a1b219e2cb23595fd
EDDY <eddmunyiri@gmail.com>
2023-08-17 07:23:42 +0300

    update file

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class Item:
         self.price = price
         self.quantity = quantity
         
-        #Actions to execute: putting all ites into a list
+        #Actions to execute: putting all items into a list
         Item.all.append(self)
 
     def calculate_total_price(self):
@@ -35,17 +35,56 @@ class Item:
                 price = float(item.get('price')),
                 quantity = int(item.get('quantity')),
             )
-
-    
+# chackig if num is integer using a static method
+    @staticmethod
+    def is_integer(num):
+        #counting the floats that are point zero (2.0,7.0,20.0)
+        if isinstance(num,float):
+            #counting floats that are point zero using the is_integer() method
+            return num.is_integer()
+        elif isinstance (num, int):
+            return True
+        else:
+            return False
+    #using the __repr__() mmethod to provide a string representation of an object
     def __repr__(self):
-        return f"Item('{self.name}', '{self.price}','{self.quantity}')"
+        return f"{self.__class__.__name__}('{self.name}', '{self.price}','{self.quantity}')"
 
     
 
+#print(Item.is_integer(7))
 
+class Phone(Item):
+    def __init__(self, name: str, price: float, quantity, broken_phone=0):
+        #using the super method to access methods and attributes of the prent Item class
+        super().__init__(name, price, quantity)
+        #Run validations to the received argument using  assert
+        #assert price >= 0, f"price {price} is not greater than or equal to zero!"
+        #assert quantity >= 0, f"quantity {quantity} is not greater than or equal to zero!"
+        assert broken_phone >= 0, f"broken_phone {broken_phone} is not greater or equal to zero"
+        
+        # Assign to self object
+        #self.name = name
+        #self.price = price
+        #self.quantity = quantity
+        self.broken_phone = broken_phone
+        
+        
 
-Item.instantiate_from_csv()
+phone1 = Phone("samsung", 500, 5, 1)
+print(phone1.calculate_total_price())
+phone2 = Phone("apple", 700, 5, 2)
+print(phone2.calculate_total_price())
+
 print(Item.all)
+print(Phone.all)
+
+
+
+
+
+#Item.instantiate_from_csv()
+#print(Item.all)
 
 #item1 = Item('phone', 200, 5)
 #item2 = Item('laptop', 1000, 3)
